f067e3269eb52a4413ed4bd4a05d77a2678a59d9
mohamadhafiz98 <hafizabuosman98@gmail.com>
2022-01-20 18:40:03 +0800

    Delete Project Sem Last directory

--- Project Sem Last/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-        #first image
-        img_top=Image.open(r"clg_img\img\biru.png")
-        img_top=img_top.resize((650,700),Image.ANTIALIAS)
-        self.photoimg_top=ImageTk.PhotoImage(img_top)
-
-        f_lbl=Label(self.root,image=self.photoimg_top)
-        f_lbl.place(x=0,y=55,width=650, height=700)
